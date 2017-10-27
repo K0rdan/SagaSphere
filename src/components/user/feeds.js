7cc3dc5f8a4564a47d4ebfb98faafdb7d751be6c
Benjamin Christau <christau_benjamin@hotmail.com>
2017-10-27 09:08:54 +0200

    First style logic implementation

--- src/components/user/feeds.js
+++ src/components/user/feeds.js
@@ -1,20 +1,66 @@
 import React, { Component } from "react";
-import { Text, View, SectionList } from "react-native";
+import { Image, Text, View, SectionList, TouchableOpacity } from "react-native";
 import PropTypes from "prop-types";
-import { findIndex } from "lodash";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { findIndex, map } from "lodash";
 import { Loader, NotificationLevel } from "./../common/";
 import { API, Config } from "./../../utils/";
 import Page from "./../page";
+
+const styles = {
+  feedSectionHeaderContainer: {
+    height: 28,
+    backgroundColor: "green"
+  },
+  feedSectionHeaderText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  feedSectionItemContainer: {
+    height: 50,
+    backgroundColor: "rgb(100,100,100)",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  feedSectionItemIconContainer: {
+    marginVertical: 5,
+    marginLeft: 5,
+    marginRight: 2.5,
+    flex: 0.1
+  },
+  feedSectionItemIcon: {
+    width: 40,
+    height: 40,
+    borderColor: "red",
+    borderWidth: 2
+  },
+  feedSectionItemTitleContainer: {
+    flex: 0.8,
+    marginHorizontal: 2.5,
+    marginVertical: 5,
+    borderColor: "red",
+    borderWidth: 2
+  },
+  feedSectionItemPlayButton: {
+    marginHorizontal: 2.5,
+    marginVertical: 5,
+    flex: 0.1,
+    borderColor: "red",
+    borderWidth: 2
+  }
+};
 
 export class Feeds extends Component {
     constructor(props) {
       super(props);
 
       this.formatFeeds = this.formatFeeds.bind(this);
-
+      this.imageLoaders = [];
       this.state = {
-        user: this.props.navigation.state.params.user || null,
-        showNotification: null
+        dataSource: null,
+        sagas: null,
+        showNotification: null,
+        user: this.props.navigation.state.params.user || null
       };
     }
 
@@ -70,18 +116,40 @@ export class Feeds extends Component {
     }
 
     renderContent() {
-      const renderFeedsRow = rowData => (
-        <View><Text>{rowData.item.title}</Text></View>
+      const renderFeedsSectionHeader = section => (
+        <View style={styles.feedSectionHeaderContainer}>
+          <Text style={styles.feedSectionHeaderText}>
+            {section.section.sectionTitle}
+          </Text>
+        </View>
       );
 
-      const renderFeedsSectionHeader = section => (
-        <View><Text>{section.section.sectionTitle}</Text></View>
+      const renderFeedsSectionItem = rowData => (
+        <View style={styles.feedSectionItemContainer}>
+          <View style={styles.feedSectionItemIconContainer}>
+            { this.renderFeedsSectionItemIcon(rowData) }
+          </View>
+          <View style={styles.feedSectionItemTitleContainer}>
+            <Text style={{ height: "100%" }}>
+              {rowData.item.title}
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={styles.feedSectionItemPlayButton}
+            onPress={null}
+          >
+            <Icon
+              name="play-arrow"
+              size={40}
+            />
+          </TouchableOpacity>
+        </View>
       );
 
       if (this.state && this.state.dataSource) {
         return (
           <SectionList
-            renderItem={renderFeedsRow}
+            renderItem={renderFeedsSectionItem}
             renderSectionHeader={renderFeedsSectionHeader}
             sections={this.state.dataSource}
             stickySectionHeadersEnabled={true}
@@ -90,6 +158,55 @@ export class Feeds extends Component {
       }
 
       return <Loader />;
+    }
+
+    renderFeedsSectionItemIcon(rowData) {
+      return (
+        <View style={{ position: "relative" }}>
+          { rowData.item.imageLoadingState === "start" ?
+              <Loader style={{ position: "absolute" }}/> :
+              null
+          }
+          { rowData.item.imageLoadingState !== "timeout" ?
+              <Image
+                source={{ uri: rowData.item.image }}
+                resizeMode="contain"
+                style={styles.feedSectionItemIcon}
+                onLoadStart={() => this.imageLoadingHandler(rowData.item, "start")}
+                onLoadEnd={() => this.imageLoadingHandler(rowData.item, "end")}
+              /> :
+              <Icon name="queue-music" size={40} />
+          }
+        </View>
+      );
+    }
+
+    imageLoadingHandler(rowDataItem, loadState) {
+      this.updateDatasourceLoadingState(rowDataItem, loadState);
+      const { dataSource } = this.state;
+      map(dataSource, (section, sectionIndex) => {
+        const sagaIndex = findIndex(section.data, saga => rowDataItem === saga);
+        if (sagaIndex !== -1) {
+          dataSource[sectionIndex].data[sagaIndex].imageLoadingState = loadState;
+          this.setState({ dataSource });
+        }
+      });
+
+      // Init 'setTimeout' when we 'start' loading an image
+      if (loadState === "start") {
+        this.imageLoaders[rowDataItem.id] = {
+          timeoutID: setTimeout(
+            this.imageLoadingHandler.bind(this),
+            Config.Network.timeout,
+            rowDataItem,
+            "timeout"
+          )
+        };
+      }
+      // Clear the timeout when we "end" loading
+      else {
+        clearTimeout(this.imageLoaders[rowDataItem.id].timeoutID);
+      }
     }
 }
 
