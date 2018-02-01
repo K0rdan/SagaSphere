f104753be168704e053946623d87f4e8d160b8e9
Benjamin Christau <christau_benjamin@hotmail.com>
2018-02-01 22:03:12 +0100

    [Update][Notification] Handle successive notifications.

--- src/redux/actions/notification.js
+++ src/redux/actions/notification.js
@@ -1,22 +1,34 @@
 import { Config } from "./../../utils/";
 
 export const NOTIFICATION_SHOW = "NOTIFICATION_SHOW";
-export const notificationShow = (message, level) => ({
+export const notificationShow = (message, level, intervalID = 0) => ({
   type: NOTIFICATION_SHOW,
   message,
-  level
+  level,
+  intervalID
 });
 
 export const NOTIFICATION_HIDE = "NOTIFICATION_HIDE";
 export const notificationhide = () => ({
   type: NOTIFICATION_HIDE,
   message: "",
-  level: null
+  level: null,
+  intervalID: 0
 });
 
-export const showNotification = (message, level) => (dispatch) => {
-  dispatch(notificationShow(message, level));
-  setTimeout(() => dispatch(notificationhide()), Config.NotificationDuration);
+export const showNotification = (message, level) => (dispatch, getState) => {
+  const { intervalID } = getState();
+
+  if (intervalID !== 0) {
+    clearInterval(intervalID);
+    dispatch(notificationhide());
+  }
+
+  const newIntervalID = setTimeout(
+    () => dispatch(notificationhide()),
+    Config.NotificationDuration
+  );
+  dispatch(notificationShow(message, level, newIntervalID));
 };
 
 export const NotificationActions = {
--- src/redux/reducers/notification.js
+++ src/redux/reducers/notification.js
@@ -2,12 +2,15 @@
 import { NotificationActions } from "./../actions/";
 import { initialNotificationState } from "./../constants/";
 
-export const NotificationReducer = (state = initialNotificationState, action) => {
-  const { type, message, level } = action;
+export const NotificationReducer = (
+  state = initialNotificationState,
+  action
+) => {
+  const { type, message, level, intervalID } = action;
   switch (type) {
     case NotificationActions.NOTIFICATION_SHOW:
     case NotificationActions.NOTIFICATION_HIDE:
-      return { ...state, message, level };
+      return { ...state, message, level, intervalID };
     default:
       return state;
   }
